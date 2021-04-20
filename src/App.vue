c303f5e80356e61fd5e5d2a3eefc0253817d37aa
Amin-Mirlohi-unb <smirlohi@unb.ca>
2021-04-20 06:59:08 -0400

    my commit3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to our Writing App"/>
+    <HelloWorld msg="Welcome to your Writing App"/>
   </div>
 </template>
 
